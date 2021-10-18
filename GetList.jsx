fea7c788d6e9488f7151fb17f871dce9d46c9e42
Denica Gateva <liseq2323@abv.bg>
2021-10-18 09:33:15 +0300

    Add files via upload

---
+++ GetList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+GetList.propTypes = {
+
+    studentList: PropTypes.array,
+
+};
+GetList.defaultProps = {
+    studentList: [],
+};
+
+function GetList(props) {
+    const { studentList } = props;
+    return(
+        <ul className="post-list">
+            {studentList.map>(post=>(
+             <li key={post.id}>{post.name} {post.age}</li>   
+            ))}
+        </ul>
+    );
+ }
+
+            export default GetList;
