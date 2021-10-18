42e4624a791cb4d559a00638023a96b5a2c401de
Denica Gateva <liseq2323@abv.bg>
2021-10-18 09:29:22 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect,useState } from "react";
 import './App.css';
-import Form from './Form';
+import GetList from './components/GetList';
 
 function App() {
-  return <Form />;
+
+  const [studentList,  setStudentList] = useState([]);
+  useEffect(()=>{
+    async function fetchStudentList() {
+      try { 
+        const requesUrl = 'https://616be81116c3fa001717181a.mockapi.io/api/student';
+        const reponse = await fetch(requesUrl);
+        const reponseJSON = await reponse.json();
+        console.log(reponseJSON);
+        setStudentList(reponseJSON);
+      } catch {
+
+      }
+    }
+    fetchStudentList();
+  }, []);
+
+  
+  return (
+   <div>
+      < GetList /> 
+      </div>
+  );
 }
 
 export default App;
